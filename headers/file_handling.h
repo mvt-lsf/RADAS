e4d815bbed7c66a48ca7f78ddff55b597483728e
sanflan <sandalinasfrancisco@gmail.com>
2019-04-09 13:31:40 -0300

    File handler

--- headers/file_handling.h
+++ headers/file_handling.h
@@ -1,2 +1,10 @@
+#ifndef main
+#define main
+#include <main.h>
+#include <stddef.h>
+#include <stdbool.h>
+#endif
+
 int write_config();
 struct th_Data read_config();
+void read_string(struct th_Data *data, char *line, char *target);
